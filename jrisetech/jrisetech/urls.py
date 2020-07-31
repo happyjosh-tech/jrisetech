45c0db58bba649645e14d0eb27b4add1ff802e15
Chukwuebuka J.O <53452638+happyjosh-tech@users.noreply.github.com>
2020-07-30 19:54:17 -0700

    Folder containing jrisetech project on firebase

---
+++ jrisetech/jrisetech/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from courses.views import CourseListView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+	path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
+	path('', CourseListView.as_view(), name='course_list'),
+	path('students/', include('students.urls')),
+	path('api/', include('courses.api.urls', namespace='api')),
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL,
+							document_root=settings.MEDIA_ROOT)
